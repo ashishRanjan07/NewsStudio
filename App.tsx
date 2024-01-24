b6c14f12426d58e3505e8ec7ddc260b546df5077
Ashish Ranjan <52354895+ashish-monal@users.noreply.github.com>
2024-01-24 20:02:35 +0530

    Android Cloud Messesing and Notification

--- App.tsx
+++ App.tsx
@@ -1,9 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React,{useEffect} from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigation from './src/navigation/stack/StackNavigation';
-
+import messaging from '@react-native-firebase/messaging';
 const App = () => {
+
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  useEffect(() => {
+    requestUserPermission()
+    getToken()
+  })
+  const getToken = async() => {
+    const token = await messaging().getToken()
+    console.log("Token =",token)
+  }
   return (
     <NavigationContainer>
     <StackNavigation/>
